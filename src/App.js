294806e84c97e098e50837812db19724b532b0d6
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-22 11:58:06 -0300

    map data 2

--- src/App.js
+++ src/App.js
@@ -19,6 +19,10 @@ function App() {
           return (
             <article key={id}>
               <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight />
             </article>
           );
         })}
